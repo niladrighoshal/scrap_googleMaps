647c95b037d490261bafb345b76759ce84047366
NILADRI GHOSHAL <niladrighoshal.20@gmail.com>
2023-08-25 23:39:04 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 queries = [
     {
         "keyword": "dentists in mumbai",
-        "max_results" : 50,
+        "max_results" : 50,  #  the number of results you want
     },
 ]
 
-number_of_scrapers = 5
+number_of_scrapers = 5  #  the number of browser instances your pc can handle
